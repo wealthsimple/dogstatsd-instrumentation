9c77c3edc23d907133481a0cbe7b201b2de7c9a9
Marco Costa <mmarcottulio@gmail.com>
2017-01-16 18:14:04 -0500

    Delete aws_cron.rb

--- lib/aws_cron.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require "aws_cron/identity"
-require "aws_cron/controller"
-require "aws_cron/runner"
-require "aws_cron/configuration"
-require "aws_cron/exception_handler"
-require "aws_cron/logger"
